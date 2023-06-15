41717b1fef93a40aded49eb5f71b1abd782f2f2c
392781 <rlencevicius@gmail.com>
2023-06-15 00:36:34 -0700

    updated build procedure to circumvent claat and gh-pages issues

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
-gem "jekyll", "~> 4.3" # installed by `gem jekyll`
-# gem "webrick"        # required when using Ruby >= 3 and Jekyll <= 4.2.2
+gem "github-pages", "~> 228", group: :jekyll_plugins
+# gem "jekyll", "3.9.3" # installed by `gem jekyll`
+gem "webrick"        # required when using Ruby >= 3 and Jekyll <= 4.2.2
 
 gem "just-the-docs", "0.5.0" # pinned to the current release
 # gem "just-the-docs"        # always download the latest release
--- wiki.sh
+++ wiki.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-if [ ! -f ./claat-linux-amd64 ]  # if claat tool doesn't exist then download and setup
+if [ ! -f ./claat ]  # if claat tool doesn't exist then download and setup
 then
-    wget https://github.com/googlecodelabs/tools/releases/download/v2.2.5/claat-linux-amd64
-    chmod u+x claat-linux-amd64
+    sudo apt install golang
+    git clone --depth 1 git@github.com:googlecodelabs/tools.git
+    cd tools/claat/
+    go get ./
+    make
+    cd bin/
+    mv claat ../../../
+    rm -rf tools/
 fi
     
-grep ' https://docs.google.com' README.md | # extract all lines with `https://docs.google.com`
+grep 'https://docs.google.com' README.md | # extract all lines with `https://docs.google.com`
     grep 'document' | # to exclude lines like the command in previous line, also search for `document` in the same line. 
     cut -d '|' -f2 | # split at `|` to separate markdown table cells
     cut -d '/' -f6 | # split at `/` to extract the file IDs
     sed 's/ *$//' | # remove any whitespace
-    xargs -i ./claat-linux-amd64 export -o tmp {} # run `claat` tool for each matching line
+    xargs -i ./claat export -o tmp {} # run `claat` tool for each matching line
